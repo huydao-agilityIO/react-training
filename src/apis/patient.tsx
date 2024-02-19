26e94a6acf5c867a4e47a34c242434466c21310a
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-02-19 16:39:10 +0700

    feat: handle response api

--- src/apis/patient.tsx
+++ src/apis/patient.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from 'react-query';
 
 // Constants
-import { API_HOSPITAL_MANAGEMENT } from '@shared/constants';
+import {
+  API_HOSPITAL_MANAGEMENT,
+  LIMIT_PATIENT_TABLE
+} from '@shared/constants';
 
 // Services
 import { getData } from '@shared/services';
@@ -9,10 +12,30 @@ import { getData } from '@shared/services';
 // Types
 import { Patient } from '@shared/types';
 
+const getDatabyPage = (page: number) =>
+  getData(
+    `${API_HOSPITAL_MANAGEMENT.HOSPITAL_MANAGEMENT_PATIENT}?page=${page}&limit=${LIMIT_PATIENT_TABLE}`
+  );
+
 export const useGetTablePatient = () => {
   const { data, error, isLoading, ...rest } = useQuery<Patient[]>({
     queryFn: () => getData(API_HOSPITAL_MANAGEMENT.HOSPITAL_MANAGEMENT_PATIENT),
     queryKey: ['patient']
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+    ...rest
+  };
+};
+
+export const useGetTablePatientByPagination = (page: number) => {
+  const { data, error, isLoading, ...rest } = useQuery<Patient[]>({
+    queryFn: () => getDatabyPage(page),
+    queryKey: ['patient', page],
+    keepPreviousData: true
   });
 
   return {
