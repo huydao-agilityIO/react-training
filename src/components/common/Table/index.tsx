f1a8b4fa2c719436df2db2594c5e8e56b6a58316
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-02-19 16:42:36 +0700

    feat: handle conflict

--- src/components/common/Table/index.tsx
+++ src/components/common/Table/index.tsx
@@ -1,5 +1,6 @@
 import { memo, useCallback } from 'react';
 import {
+  Center,
   Spinner,
   Table,
   TableCaption,
@@ -14,17 +15,23 @@ import {
 import { ContentMapping, HeadingMapping } from '@shared/types';
 
 // Components
-import { TextHelper } from '@shared/components';
+import { Pagination, TextHelper } from '@shared/components';
 
 interface DataTableProps {
   data: ContentMapping[][];
+  currentPage: number;
+  totalPage: number;
   headingMapping?: HeadingMapping[];
   isLoading?: boolean;
+  onChangePage: (pageNumber: number) => void;
 }
 
 const DataTable = ({
   data,
   headingMapping = [],
+  currentPage,
+  totalPage,
+  onChangePage,
   isLoading = false
 }: DataTableProps) => {
   const renderBodyTable = useCallback(() => {
@@ -45,17 +52,28 @@ const DataTable = ({
     }
 
     return (
-      <Tbody>
-        {data.map((contentMapping: ContentMapping[], index) => (
-          <Tr key={`HAD_${index}`}>
-            {contentMapping.map(({ customRender, value, cellStyle }) => (
-              <Td {...cellStyle}>{value ?? customRender?.()}</Td>
-            ))}
-          </Tr>
-        ))}
-      </Tbody>
+      <>
+        <Tbody>
+          {data.map((contentMapping: ContentMapping[], index) => (
+            <Tr key={`HAD_${index}`}>
+              {contentMapping.map(({ customRender, value, cellStyle }) => (
+                <Td {...cellStyle}>{value ?? customRender?.()}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+        <TableCaption>
+          <Center marginTop={5}>
+            <Pagination
+              currentPage={currentPage}
+              totalPage={totalPage}
+              onChangePage={onChangePage}
+            />
+          </Center>
+        </TableCaption>
+      </>
     );
-  }, [data, isLoading]);
+  }, [currentPage, data, isLoading, onChangePage, totalPage]);
 
   return (
     <TableContainer w={{ sm: 'full', xl: '6xl' }} bg="light.300">
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,18 +1,45 @@
-import { Stack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 // Apis
-import { useGetTablePatient } from '@shared/apis';
+import {
+  useGetTablePatient,
+  useGetTablePatientByPagination
+} from '@shared/apis';
+
+// Constants
+import { LIMIT_PATIENT_TABLE } from '@shared/constants';
+
+// Layouts
+import { DashboardLayout } from '@shared/layouts';
 
 // Components
 import TablePatient from '@shared/pages/HomePage/TablePatient';
 
 const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  // Fetch all data to calc page number
   const { data: dataPatient, isLoading } = useGetTablePatient();
 
+  // Fetch data with param: page and limit
+  const { data: dataPatientByPagination } =
+    useGetTablePatientByPagination(currentPage);
+
+  const totalPage = Math.ceil((dataPatient ?? []).length / LIMIT_PATIENT_TABLE);
+
+  const handleChangePage = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <Stack padding={7.5}>
-      <TablePatient data={dataPatient ?? []} isLoading={isLoading} />
-    </Stack>
+    <DashboardLayout onSearch={() => {}}>
+      <TablePatient
+        data={dataPatientByPagination ?? []}
+        currentPage={currentPage}
+        isLoading={isLoading}
+        totalPage={totalPage}
+        onChangePage={handleChangePage}
+      />
+    </DashboardLayout>
   );
 };
 
