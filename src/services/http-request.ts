6a0d5ee225fc2954624bc41adb9c71e54bab5a33
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-02-22 19:04:21 +0700

    feat: add delete services

--- src/services/http-request.ts
+++ src/services/http-request.ts
@@ -48,3 +48,26 @@ export async function postData<T>(url: string, dataField: T) {
 
   return response;
 }
+
+/**
+ * The function post data
+ *
+ * @param url - The API
+ */
+export async function deleteData(url: string) {
+  const response = await fetch(url, {
+    method: METHOD.DELETE,
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((res) => {
+      if (!res.ok) {
+        return res.text();
+      }
+      return res.json();
+    })
+    .catch(() => {
+      throw new Error(ERROR_MESSAGES_SERVICES.END_POINT);
+    });
+
+  return response;
+}
