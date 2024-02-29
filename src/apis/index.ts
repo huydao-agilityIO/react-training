b4a986dfeb8a7b4bab7bbaeb5d9083a5fedb0d6b
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-02-29 09:26:30 +0700

    Integrate API hospital staff list

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,3 @@
 export * from './auth';
+export * from './hospitalStaff';
 export * from './patient';
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,15 @@
 // Layouts
 import { DashboardLayout } from '@shared/layouts';
+
+// Components
+import HospitalStaffLayout from '@shared/pages/HomePage/HospitalStaffLayout';
 import TablePatientLayout from '@shared/pages/HomePage/TablePatientLayout';
 
 const HomePage = () => {
   return (
     <DashboardLayout onSearch={() => {}}>
       <TablePatientLayout />
+      <HospitalStaffLayout />
     </DashboardLayout>
   );
 };
